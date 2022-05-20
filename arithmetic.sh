4e69998f311a229e6ed9b4d4f449de69cff27c63
devksom <dev@theinfoseclab.com>
2022-05-20 03:57:44 +0000

    Program to add , subtract, divide and multiply x and y without constraint

---
+++ arithmetic.sh
@@ -0,0 +1,11 @@
+!/bin/bash
+echo "Please Enter X"
+read x
+echo "Please Enter Y"
+read y
+
+echo "Addition" $((x+y));
+echo "Substraction" $((x-y));
+echo "Multiplication" $((x*y));
+echo "Division" $((x/y));
+
