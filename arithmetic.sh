4f004145b27936c0ef46f23cd1036dca60a9c809
Stephen Somuah <dev@theinfoseclab.com>
2022-05-25 05:32:32 +0000

    Comments Added

--- arithmetic.sh
+++ arithmetic.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+#Take inputs from user
 echo "Please Enter X"
 read x
 echo "Please Enter Y"
 read y
-
+#Display Results of all arithmetic operations
 echo "Addition" $((x+y));
 echo "Substraction" $((x-y));
 echo "Multiplication" $((x*y));
